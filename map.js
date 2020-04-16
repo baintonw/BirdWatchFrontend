193ff568b04ad67cc26c753e8e35ab322be3c2f4
Will Bainton <baintonw@gmail.com>
2020-04-15 23:12:33 -0400

    skeleton for bird info window added

--- map.js
+++ map.js
@@ -17,7 +17,7 @@ let search = false;
 //Bird search initializes map, which 
 birdBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  alert(`One moment while we look for your bird: ${selected.comName}`)
+  alert(`One moment while we look for your bird: ${selected.comName }`)
   search = true;
   initMap();
 })
@@ -139,21 +139,11 @@ async function fetchBird(selectedBird) {
   var contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      `<h1 id="firstHeading" class="firstHeading">${selected.comName}</h1>`+
+      `<p><i>${selected.sciName}</i></p>`+ 
       '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
+      `<p>Seen at this location: ${selected.locName}</p>`+
+      `<p>Number seen: ${selected.howMany}</p>`+
       '</div>'+
       '</div>';
 
